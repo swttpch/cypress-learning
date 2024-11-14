1f90ed8de56d4de7ff0233f5483bf12ed0122a50
swttpch <igor.souza@ecto.digital>
2024-11-14 15:41:25 -0300

    chore: testing grudge list

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -19,12 +19,52 @@ describe('Various examples', () => {
     cy.location('pathname').should('eq', '/best-practices')
   })
 
-  it.only('intercepts', () => {
+  it('intercepts', () => {
     cy.intercept("POST", "http://localhost:3000/examples", {
       // like mocked data in fixtures foulder
       fixture: "example.json",
     })
 
     cy.get('[data-test="post-button"]').click()
+  })
+
+  it.only('grudge list', () => {
+    cy.get('[data-test="grudge-list-title"]').should('contain.text', 'Add Some Grudges')
+    cy.get('[data-test="grudge-list"]').within(() => {
+      cy.get('li').should('have.length', 0)
+    })
+
+    cy.get('[data-test="clear-button"]').should('not.exist')
+    cy.get('[data-test="grudge-input"]').within(() => {
+      cy.get('input').type('first grudge')
+    })
+    cy.get('[data-test="add-grudge-button"]').click()
+    cy.get('[data-test="grudge-list"]').within(() => {
+      cy.get('li').should('have.length', 1)
+    })
+    cy.get('[data-test="grudge-list-title"]').should('contain.text', 'Grudges')
+    cy.get('[data-test="grudge-input"]').within(() => {
+      cy.get('input').type('second grudge')
+    })
+    cy.get('[data-test="add-grudge-button"]').click()
+
+    cy.get('[data-test="grudge-list"]').within(() => {
+      cy.get('li').should('have.length', 2)
+      cy.get('li').its(0).should('contain.text', 'first grudge')
+    })
+    cy.get('[data-test="grudge-list"]').within(() => {
+      cy.get('li').its(0).within(() => {
+        cy.get('button').click()
+      })
+    })
+    cy.get('[data-test="grudge-list"]').within(() => {
+      cy.get('li').should('have.length', 1)
+      cy.get('li').its(0).should('contain.text', 'second grudge')
+    })
+
+    cy.get('[data-test="clear-button"]').click()
+    cy.get('[data-test="grudge-list"]').within(() => {
+      cy.get('li').should('have.length', 0)
+    })
   })
 })
